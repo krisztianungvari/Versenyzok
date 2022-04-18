7a39a4b1316206bfe10612b32c6aece7cfb46a93
Ungvári Krisztián <krisz270@gmail.com>
2022-04-18 12:32:30 +0200

    Add project files.

---
+++ Versenyzok/Pilota.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versenyzok
+{
+    class Pilota
+    {
+        //név;születési_dátum;nemzetiség;rajtszám
+        string nev;
+        DateTime szuletesi_datum;
+        string nemzetiseg;
+        byte rajtszam;
+
+        public Pilota (string sor)
+        {
+            string[] daraboltSor = sor.Split(';');
+            Nev = daraboltSor[0];
+            Szuletesi_datum = Convert.ToDateTime(daraboltSor[1]);
+            Nemzetiseg = daraboltSor[2];
+            if (!string.IsNullOrEmpty(daraboltSor[3]))
+            {
+                Rajtszam = Convert.ToByte(daraboltSor[3]); 
+            }
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Szuletesi_datum { get => szuletesi_datum; set => szuletesi_datum = value; }
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public byte Rajtszam { get => rajtszam; set => rajtszam = value; }
+    }
+}
---
+++ Versenyzok/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Versenyzok
+{
+    class Program
+    {
+        static List<Pilota> pilotak = new List<Pilota>();
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Feladat03();
+            Feladat04();
+            Feladat05();
+            Feladat06();
+            Feladat07();
+
+            Console.WriteLine();
+            Console.WriteLine("Program vége!");
+            Console.ReadKey();
+        }
+
+        private static void Feladat07()
+        {
+            Dictionary<byte, int> rajtszamok = new Dictionary<byte, int>();
+            foreach (Pilota pilota in pilotak)
+            {
+                if (pilota.Rajtszam != 0)
+                {
+                    if (rajtszamok.ContainsKey(pilota.Rajtszam))
+                    {
+                        rajtszamok[pilota.Rajtszam]++;
+                    }
+                    else
+                    {
+                        rajtszamok.Add(pilota.Rajtszam, 1);
+                    }
+                }
+            }
+            //Key    - rajtszám értéke
+            //Value  - rajtszám daraszáma
+            Console.Write("7. Feladat: ");
+            foreach (var rajtszam in rajtszamok)
+            {
+                if (rajtszam.Value > 1)
+                {
+                    Console.Write($" {rajtszam.Key},");
+                }
+            }
+
+        }
+
+        private static void Feladat06()
+        {
+            Pilota legkisebbRajtszamupilota = pilotak.First();
+            foreach  (Pilota pilota in pilotak)
+            {
+                if (pilota.Rajtszam != 0 && pilota.Rajtszam < legkisebbRajtszamupilota.Rajtszam)
+                {
+                    legkisebbRajtszamupilota = pilota;
+                }
+            }
+            Console.WriteLine($"6. feladat: {legkisebbRajtszamupilota.Nemzetiseg}");
+        }
+
+        private static void Feladat05()
+        {
+            Console.WriteLine("5. feladat: ");
+            foreach (Pilota pilota in pilotak)
+            {
+                if (pilota.Szuletesi_datum.Year < 1901)
+                {
+                    Console.WriteLine($"\t{pilota.Nev} ({pilota.Szuletesi_datum.ToShortDateString()})");
+                }
+            }
+        }
+
+        private static void Feladat04()
+        {
+            // listában tudok első és utolsót kiválasztani, plusz lehetősgéem van indexelni is, ha mondjuk a 10. versenyző adatai kellenek!
+            Pilota utolsoPilota = pilotak.Last();
+            string utolsoPilotaNeve = pilotak.Last().Nev;
+            Console.WriteLine($"4. feladat: {utolsoPilotaNeve}");
+
+            //Console.WriteLine(pilotak[0].Nev); első név
+            //Console.WriteLine(pilotak[pilotak.Count -1].Nev); utolsó neve indexeléssel || -1 mert a 0-ról indul!!
+        }
+
+        private static void Feladat03()
+        {
+            Console.WriteLine($"3. Feladat: {pilotak.Count}");
+        }
+
+        private static void Beolvasas()
+        {
+            foreach (string pilota in File.ReadAllLines("pilotak.csv").Skip(1))
+            {
+                pilotak.Add(new Pilota (pilota));
+            }
+        }
+    }
+}
